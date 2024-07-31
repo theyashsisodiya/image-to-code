c4532857abd01c428fe21911d2b3ec37f0c81a81
Yashraj Singh Sisodiya <97979728+theyashsisodiya@users.noreply.github.com>
2024-07-31 20:11:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pathlib
 from PIL import Image
 import google.generativeai as genai
 # Configure the API key directly in the script
-API_KEY = 'AIzaSyDEssPxnfAFbKOiu3T_e_N6UAbz7kZsiwY'
+API_KEY = 'AIzaSyC2i99VRaGG15tKwXBWlCC9V1U7kwjE1Ps'
 genai.configure(api_key=API_KEY)
 
 # Generation configuration
